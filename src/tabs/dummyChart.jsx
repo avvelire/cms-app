031107436acb006536d5ad942189dcef9f45ae1b
dAndrVi <vasylkivandrej@gmail.com>
2023-10-27 18:41:03 +0300

    first commit

---
+++ src/tabs/dummyChart.jsx
@@ -0,0 +1,27 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { Bar } from 'react-chartjs-2';
+import tabsData from '../data/tabs.json';
+const DummyChart = () => {
+  const data = {
+    labels: tabsData.map((data) => data.title),
+    datasets: [
+      {
+        label: 'Dummy Chart',
+        data: tabsData.map((data) => data.order),
+        backgroundColor: ['red', 'blue', 'purple'],
+        borderColor: 'green',
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  return (
+    <div className="mainContent">
+      <h1 className="mainContent__title">Dummy Chart</h1>
+      <Bar data={data} />
+    </div>
+  );
+};
+
+export default DummyChart;
